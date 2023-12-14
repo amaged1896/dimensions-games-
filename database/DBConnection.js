ecb1edb9602878d2b4628a55589db85504cc7f0c
amaged1896 <ahmed.maged1896@gmail.com>
2023-12-14 02:29:35 +0200

    "test"

--- database/DBConnection.js
+++ database/DBConnection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const dbConnection = () => {
-    mongoose.connect(process.env.DATABASE_ATLAS)
+    mongoose.connect('mongodb+srv://ahmedmaged1896:YVttUAcGmNDgsWyP@cluster0.9mdq3qq.mongodb.net/')
         .then(() => console.log('Database connection established successfully'))
         .catch((err) => console.log('Database connection failed', err.message));
 };
